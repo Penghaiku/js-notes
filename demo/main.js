13854bfcd0a7555127302bcd989cb71ca138b271
Penghaiku <haikupeng@gmail.com>
2020-12-31 12:05:14 +0800

    add demo

---
+++ demo/main.js
@@ -0,0 +1,34 @@
+const {app,BrowserWindow} = require("electron")
+const path = require("path")
+
+//  /cxxx
+//path.join("/c",'xxxxxxxx')
+
+
+const createWindow=()=>{
+    const mainWindow = new BrowserWindow({
+        width:600,
+        height:400
+    })
+    mainWindow.loadFile(path.join(__dirname,"青衫不是山.html"))
+}
+
+
+//监听应用的启动事件
+app.on("ready",createWindow)
+
+
+//监听窗口关闭的事件，关闭的时候退出应用，masOs 需要排除
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
+
+//Macos中点击dock中的应用图标的时候重新创建窗口
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+})
